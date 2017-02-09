70e3ee5d050a52ff55d329e09d8d27a4e36d46a8
rjoseph01 <robert.joseph@nyu.edu>
2017-02-09 18:14:44 -0500

    Version 2.0
    Added another page, changed ghost functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
-$('.ghost').on('mouseover', function() {
+/*$('.ghost').on('mouseover', function() {  //fades ghost on mouseover
   $('.ghost').fadeOut(1800);
 })
 
-$('.ghosttalk').on('click', function() {
+$('.ghosttalk').on('click', function() { //brings ghost back when clicked
   $('.ghost').fadeIn(2400);
+})*/
+function gFade(){  //makes ghost disappear
+	$('.ghost').fadeOut(1800);
+	setTimeout(gAppear, 5000)
+}
+function gAppear(){  //makes ghost reappear
+	$('.ghost').fadeIn(2400);
+	setTimeout(gFade, 5000)
+}
+$('.ghost').on('mouseover', function(){  //initiates fading
+	setTimeout(gFade, 500)
 })
-$('.nobut').on('click', function(){
+
+$('.mgar').on('click', function(){   //dims Dracula
+	$(".dracula").css('opacity', '0.25');
+})	
+
+$('.lgar').on('click', function(){  //revives Dracla
+	$(".dracula").css('opacity', '2.0');
+})	
+
+$('.nobut').on('click', function(){  /*owl fades in when clicked and hides monster*/
 	$('.Owl').fadeIn(1200);
   $('.monster').hide();
 })
-$('.yesbut').on('click', function(){
+$('.yesbut').on('click', function(){ /*monster pops up and owl fades when clicked*/
 	$('.monster').show();
   $('.Owl').hide();
 })
